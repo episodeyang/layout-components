80a903fe81c8a5723f1ccf47df319377f99ec447
Ge Yang <yangge1987@gmail.com>
2016-12-11 19:06:53 -0600

    BUILD DOC

--- dist/FlexSpacer.js
+++ dist/FlexSpacer.js
@@ -1,6 +1,10 @@
 "use strict";
 
 exports.__esModule = true;
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; /** Created by ge on 12/11/16. */
+
+
 exports.default = FlexSpacer;
 
 var _react = require("react");
@@ -13,9 +17,8 @@ var _FlexItem2 = _interopRequireDefault(_FlexItem);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-/** Created by ge on 12/11/16. */
 function FlexSpacer(props) {
-  return _react2.default.createElement(_FlexItem2.default, { fluid: true });
+  return _react2.default.createElement(_FlexItem2.default, _extends({ fluid: true }, props));
 }
 ;
 
